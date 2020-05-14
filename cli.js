009cb31e38c21582b3b49418a969f0c8ab5e1fab
bbecquet <benjamin.becquet@gmail.com>
2020-05-14 15:34:39 +0200

    Cleaner and simpler version with JSDOM

--- cli.js
+++ cli.js
@@ -1,29 +1,31 @@
 const fetch = require('node-fetch');
 const emojis = require('node-emoji');
 const getTweets = require('./parser').getTweets;
+const JSDOM = require('jsdom').JSDOM;
 
 const [,, threadReaderUrl] = process.argv;
 
-function processHTML(html) {
-    return getTweets(html)
-        .map(({ tweetHTML, videoHTML, images }) => {
-            const tags = [ `<p>${emojis.strip(tweetHTML)}</p>` ];
-            if (images.length > 0) {
-                tags.push(`<figure style="display:flex">${images.map(imageToHtml).join('')}</figure>`);
-            }
-            if (videoHTML) {
-                tags.push(`<figure>${videoHTML}</figure>`);
-            }
-            return tags;
-        })
-        .reduce((acc, tweetParts) => acc.concat(tweetParts), []) // alternative for missing .flat
-        .join('');
+function processDocument(document) {
+  return getTweets(document)
+    .map(({ tweetHTML, videosHTML, images }) => {
+      const tags = [ `<p>${emojis.strip(tweetHTML)}</p>` ];
+      if (images.length > 0) {
+        tags.push(`<figure style="display:flex">${images.map(imageToHtml).join('')}</figure>`);
+      }
+      if (videosHTML) {
+        tags.push(`<figure>${videosHTML.join('')}</figure>`);
+      }
+      return tags;
+    })
+    .reduce((acc, tweetParts) => acc.concat(tweetParts), []) // alternative for missing .flat
+    .join('');
 }
 
 const imageToHtml = ({ url }) => 
-    `<a class="media-link" href="${url}"><img class="media" loading="lazy" src="${url}" alt="" /></a>`;
+  `<a class="media-link" href="${url}"><img class="media" loading="lazy" src="${url}" alt="" /></a>`;
 
 fetch(threadReaderUrl)
-    .then(response => response.text())
-    .then(processHTML)
-    .then(console.log);
+  .then(response => response.text())
+  .then(html => new JSDOM(html).window.document)
+  .then(processDocument)
+  .then(console.log);
--- parser.js
+++ parser.js
@@ -1,46 +1,31 @@
-const cheerio = require('cheerio');
-const Entities = require('html-entities').XmlEntities;
-const entities = new Entities();
+const queryDOM = element => selector => Array.from(element.querySelectorAll(selector));
 
-const getImageData = $img => ({
-    url: $img.find('img').first().attr('data-src')
+const getImageData = img => ({
+  url: queryDOM(img)('img')[0].getAttribute('data-src')
 });
 
-const cleanHtml = html => entities.decode(html).replace(/\n/g, '');
+function parseTweet(tweet) {
+  const $tweet = queryDOM(tweet);
 
-function parseTweet($originalTweet) {
-    const $tweet = $originalTweet.clone();
+  const images = $tweet('.entity-image').map(getImageData);
+  const videosHTML = $tweet('.entity-video').map(video => video.innerHTML);
 
-    const images = $tweet
-        .find('.entity-image')
-        .map((_index, img) => getImageData(cheerio(img))).get();
-    
-    const videos = $tweet
-        .find('.entity-video')
-        .map((_index, video) => cheerio(video).html()).get();
-        
-    $tweet.children('.row')
-        .add($tweet.children('.entity-image'))
-        .add($tweet.children('.entity-video'))
-        .add($tweet.children('.tw-permalink'))
-        .remove();
+  // Clean stuff. /!\ Modify the original node.
+  const ignoreSelector = ['.row', '.entity-image', '.entity-video', '.tw-permalink'].join(',');
+  $tweet(ignoreSelector).forEach(child => child.remove());
+  $tweet('a.entity-url').forEach(child => {
+    child.removeAttribute('data-preview');
+    child.removeAttribute('class');
+  });
+  const tweetHTML = tweet.innerHTML; 
 
-    $tweet.children('a.entity-url')
-        .removeAttr('data-preview')
-        .removeAttr('class');
-    
-    return {
-        tweetHTML: cleanHtml($tweet.html()),
-        videoHTML: videos[0],
-        images,
-    };        
+  return { tweetHTML, images, videosHTML };
 }
 
-function getTweets(html) {
-    const $ = cheerio.load(html, { decodeEntities: false });    // decodeEntities: false has no effect -_-
-    return $('.t-main .content-tweet')
-        .map((_index, tweet) => cheerio(tweet)).get()
-        .map(parseTweet);
+function getTweets(threadReaderDoc) {
+  return queryDOM(threadReaderDoc)('.t-main .content-tweet').map(parseTweet);
 }
 
-exports.getTweets = getTweets;
+if (typeof exports === 'object' && typeof module !== 'undefined') {
+  exports.getTweets = getTweets;
+}
