4043306e811efa037687165aa67d8b42c441555c
bbecquet <benjamin.becquet@gmail.com>
2020-05-27 16:50:53 +0200

    Working resource downloader

--- cli.js
+++ cli.js
@@ -2,29 +2,33 @@ const fetch = require('node-fetch');
 const emojis = require('node-emoji');
 const getTweets = require('./parser').getTweets;
 const JSDOM = require('jsdom').JSDOM;
-const util = require('util')
-const fs = require('fs')
-const streamPipeline = util.promisify(require('stream').pipeline)
+const fs = require('fs-extra');
+const path = require('path');
 
-const [,, threadReaderUrl] = process.argv;
+const [,, threadReaderUrl, rsrcDirPath, rsrcUrlPath] = process.argv;
 
-async function download(url, localPath) {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
-  await streamPipeline(response.body, fs.createWriteStream(localPath));
-}
-
-function processDocument(document) {
+function processDocument(document, { extractResources = false, resourcesUrlPath = '.' }) {
   const resources = [];
+  const urlTransformer = extractResources ? rewriteUrl(resourcesUrlPath) : id;
   const html = getTweets(document)
-    .map(({ tweetHTML, videos, images }) => {
+    .map(({ tweetHTML, images, videos }) => {
       const tags = [ `<p>${emojis.strip(tweetHTML)}</p>` ];
       if (images.length > 0) {
-        images.forEach(image => resources.push(image));
-        tags.push(`<figure style="display:flex">${images.map(imageToHtml).join('')}</figure>`);
+        if (extractResources) {
+          images.forEach(img => resources.push(img.url));
+        }
+        tags.push(`<figure style="display:flex">
+          ${images.map(imageToHtml(urlTransformer)).join('')}
+        </figure>`);
       }
       if (videos) {
-        tags.push(`<figure>${videos.map(videoToHtml).join('')}</figure>`);
+        if (extractResources) {
+          videos.forEach(video => {
+            resources.push(video.poster);
+            video.sources.forEach(source => resources.push(source.src));
+          });
+        }
+        tags.push(`<figure>${videos.map(videoToHtml(urlTransformer)).join('')}</figure>`);
       }
       return tags;
     })
@@ -34,33 +38,42 @@ function processDocument(document) {
   return { html, resources };
 }
 
-
-async function downloadResources(resources) {
-  resources.forEach(async ({ url }, index) => {
-    console.log(`Downloading ${url}…`);
-    await download(url, `./${index}.jpg`);
-  });
+function download(url, destPath) {
+  return fetch(url)
+    .then(response => response.buffer())
+    .then(buffer => fs.outputFile(destPath, buffer));
 }
 
+function downloadResources(urls, localPath) {
+  return Promise.all(urls.map(async url => {
+    console.log(`Downloading ${url}…`);
+    return download(url, path.join(localPath, resourceBaseName(url)));
+  }));
+}
 
-const imageToHtml = ({ url }) => 
-  `<a class="media-link" href="${url}"><img class="media" loading="lazy" src="${url}" alt="" /></a>`;
+const id = x => x;
+const resourceBaseName = url => path.basename(new URL(url).pathname);
+const rewriteUrl = urlPath => url => path.join(urlPath, resourceBaseName(url));
 
-const videoToHtml = ({ poster, sources }) => {
-  const types = sources.map(({ src, type }) => `<source src="${src}" type="${type}" />`);
-  return `<video controls="" poster="${poster}">
+const imageToHtml = urlTransformer => ({ url }) => 
+  `<a class="media-link" href="${urlTransformer(url)}">
+    <img class="media" loading="lazy" src="${urlTransformer(url)}" alt="" />
+  </a>`;
+
+const videoToHtml = urlTransformer => ({ poster, sources }) => {
+  const types = sources.map(({ src, type }) => `<source src="${urlTransformer(src)}" type="${type}" />`);
+  return `<video controls="" poster="${urlTransformer(poster)}">
     ${types.join('')}
-    <img alt="" src="${poster}" />
+    <img alt="" src="${urlTransformer(poster)}" />
   </video>`;
 }
 
 fetch(threadReaderUrl)
   .then(response => response.text())
   .then(html => new JSDOM(html).window.document)
-  .then(processDocument)
-  .then(({ html, resources }) => {
-//    console.log(resources)
-    downloadResources(resources);
+  .then(doc => processDocument(doc, { extractResources: rsrcDirPath, resourcesUrlPath: rsrcUrlPath || rsrcDirPath }))
+  .then(async ({ html, resources }) => {
+    await downloadResources(resources, rsrcDirPath);
     return html;
-  });
-  // .then(console.log);
+  })
+  .then(console.log);
